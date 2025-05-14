1c2f4959a7afbba79991e422a14ddcea9884c860
Hichem <78042750+indra100x@users.noreply.github.com>
2025-05-14 21:15:10 +0200

    Update CashierResource.php

--- app/Http/Resources/CashierResource.php
+++ app/Http/Resources/CashierResource.php
@@ -17,6 +17,7 @@ class CashierResource extends JsonResource
         return [
             'id' => $this->id,
             'name' => $this->name,
+             'email'=> $this->email,
         ];
     }
 }
